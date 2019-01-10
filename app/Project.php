7ef291a06a6af51bffdfc4ea75f7acd1e28356ae
Sebastian Awatramani <sebastian.awatramani+github@gmail.com>
2019-01-10 10:46:23 -0700

    Suppress multiple definitions for class errors in IDE
    Ignored _ide_helper_models.php to get this to stop throwing a multiple
    definitions for class warning

--- app/Project.php
+++ app/Project.php
@@ -6,6 +6,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+//Ignored _ide_helper_models.php to get this to stop throwing a multiple definitions for class warning
+
 class Project extends Model
 {
     //
